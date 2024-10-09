83a313b2aad2679d89d8d1fdcb792ebb65127e10
iliaschair <iliaschair14@gmail.com>
2024-10-09 13:20:51 +0200

    add "EquiformerV2S_OC20_DenoisingPos" to __init__.py

--- src/fairchem/core/models/equiformer_v2/__init__.py
+++ src/fairchem/core/models/equiformer_v2/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
 from .equiformer_v2_deprecated import EquiformerV2
+from .equiformer_v2_denoising import EquiformerV2S_OC20_DenoisingPos
 
-__all__ = ["EquiformerV2"]
+__all__ = ["EquiformerV2", EquiformerV2S_OC20_DenoisingPos]
