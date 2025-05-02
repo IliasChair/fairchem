436809227f0f61c4c152fc39f47dee6cde27a2d7
iliaschair <iliaschair14@gmail.com>
2025-05-02 23:25:42 +0200

    Update denoising position metrics handling in dens_trainer to account for 'forces' in denoising targets. Adjust conditions for metrics updates based on noise mask presence.

--- src/fairchem/core/models/equiformer_v2/trainers/dens_trainer.py
+++ src/fairchem/core/models/equiformer_v2/trainers/dens_trainer.py
@@ -173,11 +173,11 @@ def denoising_pos_eval(
         res = mae(prediction, target, target_name)
         metrics = evaluator.update(f"denoising_{target_name}_mae", res, metrics)
 
-    if target.get("noise_mask") is None:
+    if target.get("noise_mask") is None and "forces" in denoising_targets:
         # Only update`denoising_pos_mae` during denoising positions if not using partially corrupted structures
         res = mae(prediction, target, "forces")
         metrics = evaluator.update("denoising_pos_mae", res, metrics)
-    else:  # Update `denoising_pos_mae` and `denoising_force_mae` if using partially corrupted structures
+    elif "forces" in denoising_targets:  # Update `denoising_pos_mae` and `denoising_force_mae` if using partially corrupted structures
         # separate S2EF and denoising positions results based on `noise_mask`
         target_tensor = target["forces"]
         prediction_tensor = prediction["forces"]
